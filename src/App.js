825ee8049d0135f561928dfcfff60cc958422725
aei <ashmee90@gmail.com>
2019-04-21 01:49:47 +0500

    added diffrent data structures in jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
+      const title = "Заголовок - переменная.";
+      const truth = true;
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +14,22 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+
+            <h1 style={{color:'tomato',margin:0}}> {title}</h1>
+            <p style={{margin:10}}>Это {11} урок из {30}. Осталось {30-11} уроков.</p>
+
+            {truth &&
+            <p style={{margin:10}}>Пробуем логические операции</p>
+            }
+            <p style={{margin:10}}> undefined {undefined} , null  {null}, true  {true} и false {false} не будут выводиться в разметку</p>
+
+            <p style={{margin:10}}> undefined {undefined} , null  {null}, true  {true} и false {false} не будут выводиться в разметку</p>
+
+            <div className="app__block">Тернарные операторы:
+                { (truth) ?
+                    <p style={{margin:10}}>Всё ок</p> :
+                    <p style={{margin:10}}>Всё плохо</p> }
+            </div>
           <a
             className="App-link"
             href="https://reactjs.org"
