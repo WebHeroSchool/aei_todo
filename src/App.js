26ec163d149c6158abc958d2f5feb09884c12860
aei <ashmee90@gmail.com>
2019-04-21 16:10:23 +0500

    fix variables

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const title = "Заголовок - переменная.";
+const truth = true;
+
 class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
 
-  render() {
-      const title = "Заголовок - переменная.";
-      const truth = true;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+                    <h1 style={{color: 'tomato', margin: 0}}> {title}</h1>
+                    <p style={{margin: 10}}>Это {11} урок из {30}. Осталось {30 - 11} уроков.</p>
 
-            <h1 style={{color:'tomato',margin:0}}> {title}</h1>
-            <p style={{margin:10}}>Это {11} урок из {30}. Осталось {30-11} уроков.</p>
+                    {truth &&
+                    <p style={{margin: 10}}>Пробуем логические операции</p>
+                    }
+                    <p style={{margin: 10}}> undefined {undefined} , null {null}, true {true} и false {false} не будут
+                        выводиться в разметку</p>
 
-            {truth &&
-            <p style={{margin:10}}>Пробуем логические операции</p>
-            }
-            <p style={{margin:10}}> undefined {undefined} , null  {null}, true  {true} и false {false} не будут выводиться в разметку</p>
+                    <p style={{margin: 10}}> undefined {undefined} , null {null}, true {true} и false {false} не будут
+                        выводиться в разметку</p>
 
-            <p style={{margin:10}}> undefined {undefined} , null  {null}, true  {true} и false {false} не будут выводиться в разметку</p>
-
-            <div className="app__block">Тернарные операторы:
-                { (truth) ?
-                    <p style={{margin:10}}>Всё ок</p> :
-                    <p style={{margin:10}}>Всё плохо</p> }
+                    <div className="app__block">Тернарные операторы:
+                        {(truth) ?
+                            <p style={{margin: 10}}>Всё ок</p> :
+                            <p style={{margin: 10}}>Всё плохо</p>}
+                    </div>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
             </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
