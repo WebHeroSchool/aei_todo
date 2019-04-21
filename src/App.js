dae0d939fab16f055c5e751571244fbd01a8aca6
aei <ashmee90@gmail.com>
2019-04-21 22:57:14 +0500

    added numbers module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { count, length} from "./number";
 
 const title = "Заголовок - переменная.";
 const truth = true;
@@ -14,24 +15,20 @@ class App extends Component {
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
-
-                    <h1 style={{color: 'tomato', margin: 0}}> {title}</h1>
-                    <p style={{margin: 10}}>Это {11} урок из {30}. Осталось {30 - 11} уроков.</p>
-
-                    {truth &&
-                    <p style={{margin: 10}}>Пробуем логические операции</p>
-                    }
-                    <p style={{margin: 10}}> undefined {undefined} , null {null}, true {true} и false {false} не будут
-                        выводиться в разметку</p>
-
-                    <p style={{margin: 10}}> undefined {undefined} , null {null}, true {true} и false {false} не будут
-                        выводиться в разметку</p>
-
-                    <div className="app__block">Тернарные операторы:
-                        {(truth) ?
-                            <p style={{margin: 10}}>Всё ок</p> :
-                            <p style={{margin: 10}}>Всё плохо</p>}
-                    </div>
+                    <p style={{margin: 10}}>Общая длина: {count * length}</p>
+                    {/*<h1 style={{color: 'tomato', margin: 0}}> {title}</h1>*/}
+                    {/*{truth &&*/}
+                    {/*<p style={{margin: 10}}>Пробуем логические операции</p>*/}
+                    {/*}*/}
+                    {/*<p style={{margin: 10}}> undefined {undefined} , null {null}, true {true} и false {false} не будут*/}
+                        {/*выводиться в разметку</p>*/}
+                    {/*<p style={{margin: 10}}> undefined {undefined} , null {null}, true {true} и false {false} не будут*/}
+                        {/*выводиться в разметку</p>*/}
+                    {/*<div className="app__block">Тернарные операторы:*/}
+                        {/*{(truth) ?*/}
+                            {/*<p style={{margin: 10}}>Всё ок</p> :*/}
+                            {/*<p style={{margin: 10}}>Всё плохо</p>}*/}
+                    {/*</div>*/}
                     <a
                         className="App-link"
                         href="https://reactjs.org"
