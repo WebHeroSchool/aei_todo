d09dcdd48e1b48b291908dfdd9376afc740656f5
aei <ashmee90@gmail.com>
2019-04-22 23:42:35 +0500

    created new structure of my app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
 import React from "react";
-
-const element = (
-    <div style={{
-            textAlign: "center",
-            color: "tomato",
-            fontSize: "5em"
-        }}>
-        Hello World!
-    </div>
-);
-
-const ItemList = () => (
-    <div style={{
-    textAlign: "center",
-    color: "tomato",
-    fontSize: ".5em"
-}}>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    <ol>
-        <li>Accumsan</li>
-        <li>Commodo</li>
-        <li>Tincidunt</li>
-    </ol>
-</div>);
+import InputItem from "../src/InputItem/InputItem"
+import ItemList from "../src/ItemList/ItemList"
+import Footer from "../src/Footer/Footer"
 
 const App = () => (
-    <div  style={{
-        textAlign: "center",
+    <div style={{
         color: "red",
-        fontSize: "3em"
-    }}>I'm App!
+        fontSize: "2em"
+    }}>
+        <h1>Список дел:</h1>
+        <InputItem />
         <ItemList />
+        <Footer />
     </div>
 );
 
-export { element, ItemList, App };
+export { App };
