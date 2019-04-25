ace446a1c7800ccab82e15746a91b5ddfc21f193
aei <ashmee90@gmail.com>
2019-04-25 22:22:53 +0500

    added material-ui and textinput

--- src/Components/InputItem/InputItem.js
+++ src/Components/InputItem/InputItem.js
@@ -1,8 +1,21 @@
 import React from "react";
+import TextField from '@material-ui/core/TextField';
 
 const InputItem = () => (
-        <input placeholder={"Введите задачу здесь..."} type={"text"} />
+
+            <TextField
+                id="standard-with-placeholder"
+                label="Добавить задание"
+                placeholder="Введите ваше задание здесь"
+                margin="normal"
+                style={{ margin: 8 }}
+                fullWidth
+                variant="outlined"
+            />
         );
 
 
 export default InputItem
+
+
+
