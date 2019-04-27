f978509f39114a1990988187e671ace7b322e18f
aei <ashmee90@gmail.com>
2019-04-27 15:15:04 +0500

    refactored state in App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,30 +5,32 @@ import Footer from "../Footer/Footer"
 import styles from './App.module.css'
 
 class App extends React.Component {
+    state = {
+            items: [
+                {
+                    value: 'Выучить JS',
+                    isDone: true
+                },
+                {
+                    value: 'Сделать уборку',
+                    isDone: false
+                },
+                {
+                    value: 'Сделать уборку в репозитории',
+                    isDone: true
+                }]
+        };
+
+
     render() {
         document.body.style.margin = '0';
-
-        const items = [
-            {
-                value: 'Выучить JS',
-                isDone: true
-            },
-            {
-                value: 'Сделать уборку',
-                isDone: false
-            },
-            {
-                value: 'Сделать уборку в репозитории',
-                isDone: true
-            }];
-
 
         return (
             <div className={styles.wrap}>
                 <div className={styles.main}>
                     <h1 className={styles.title}>Список дел:</h1>
                     <InputItem />
-                    <ItemList items={items} />
+                    <ItemList items={this.state.items} />
                     <Footer taskCount={6} />
                 </div>
             </div>);
