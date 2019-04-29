c752066b5ccbbe34170da39b825d95f2fce5806c
aei <ashmee90@gmail.com>
2019-04-29 20:11:34 +0500

    added checkbox reaction

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -3,10 +3,16 @@ import Item from "../Item/Item"
 import List from '@material-ui/core/List';
 
 
-const ItemList = ({items, onClickDone}) => (
+const ItemList = ({items, onClickDone }) => (
         <List>
             {items.map(item => (
-                <Item key={item.value} value={item.value} isDone={item.isDone} onClickDone={onClickDone} />
+                <Item
+                    key={item.value}
+                    value={item.value}
+                    isDone={item.isDone}
+                    onClickDone={onClickDone}
+                    id={item.id }
+                />
             ))}
         </List>
     );
