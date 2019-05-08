9fc63a8055620341341d516a3837395884a5400a
aei <ashmee90@gmail.com>
2019-05-08 13:51:03 +0500

    default props added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,11 @@ import React from "react";
 import styles from './Footer.module.css'
 
 const Footer = ( { taskCount }) => (
-    <div className={styles.tasks}> Осталось задач: {taskCount} дел</div>
+    <div className={styles.tasks}> Осталось задач: {taskCount}</div>
 );
+
+Footer.defaultProps = {
+    count: 0
+};
 
 export default Footer
